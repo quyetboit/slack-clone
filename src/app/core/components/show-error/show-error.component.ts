c4025abeaddfe9c831377842e302a313499c3281
quyetlv <anhquyet120901@gmail.com>
2023-03-25 17:45:39 +0700

    show error common

---
+++ src/app/core/components/show-error/show-error.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AbstractControl, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-show-error',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './show-error.component.html',
+  styleUrls: ['./show-error.component.scss']
+})
+export class ShowErrorComponent {
+  @Input() control!: AbstractControl<any>;
+  @Input() controlName = '';
+
+  get keyError () {
+    console.log('Control: ', this.control)
+    const key = (this.control && this.control.errors) ? Object.keys(this.control?.errors)[0] : '';
+    return key;
+  }
+}

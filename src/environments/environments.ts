8fa3e6fd3685c2f62f0d8f4d0c035876acc69f95
quyetlv <anhquyet120901@gmail.com>
2023-04-11 07:28:40 +0700

    hide

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,13 +1,5 @@
 export const environments = {
   productions: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyDYroyA1bFbuCH_uYp1pLei71m-lIHqxCg",
-    authDomain: "chat-app-c3612.firebaseapp.com",
-    projectId: "chat-app-c3612",
-    storageBucket: "chat-app-c3612.appspot.com",
-    messagingSenderId: "1008409295994",
-    appId: "1:1008409295994:web:6475e7539b138398a2cfc3",
-    measurementId: "G-MM6L0FC7DD"
-  },
+  firebaseConfig: {} // Config firebase here,
 
 }
